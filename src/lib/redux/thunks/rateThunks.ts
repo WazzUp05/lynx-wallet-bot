5d31bfa58fe791d5c6b27dfa4c0db35d2dea4ec2
Vassiliy Rybakov <vasyaribakov1@gmail.com>
2025-09-15 20:56:34 +0500

    try

--- src/lib/redux/thunks/rateThunks.ts
+++ src/lib/redux/thunks/rateThunks.ts
@@ -2,7 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { setRates } from "../slices/rateSlice";
 
 export const fetchRates = createAsyncThunk("rate/fetchRates", async (_, { dispatch }) => {
-    const res = await fetch("http://164.90.211.61/api/rate");
+    const res = await fetch("https://stage.lynx-wallet.com/api/rate");
     const data = await res.json();
     if (data.success && Array.isArray(data.data)) {
         dispatch(setRates(data.data));
