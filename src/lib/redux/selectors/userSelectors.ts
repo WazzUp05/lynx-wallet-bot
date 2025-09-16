2298620c5690ac5dd730bef9835b3ec56bd2b95b
Vassiliy Rybakov <vasyaribakov1@gmail.com>
2025-09-16 12:48:15 +0500

    fix

--- src/lib/redux/selectors/userSelectors.ts
+++ src/lib/redux/selectors/userSelectors.ts
@@ -1,5 +1,5 @@
 import { RootState } from "../store";
 
 const getUser = (state: RootState) => state.user;
-const getUserBalance = (state: RootState) => state.user.balance;
-export { getUser, getUserBalance };
+
+export { getUser };
